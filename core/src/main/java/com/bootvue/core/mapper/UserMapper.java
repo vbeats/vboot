d5c47650f5cb7a67f67f5056ccf8f7a02290ca4b
bootvue <bootvue@gmail.com>
2021-05-07 16:04:09 +0800

    fix field name error

--- core/src/main/java/com/bootvue/core/mapper/UserMapper.java
+++ core/src/main/java/com/bootvue/core/mapper/UserMapper.java
@@ -9,7 +9,7 @@ public interface UserMapper extends BaseMapper<User> {
     @Select("select u.* from `user` u, tenant t where u.tenant_id = t.id and u.username = #{username} and u.password = #{password} and u.status = 1 and t.code = #{tenant_code} and u.delete_time is null")
     User findByUsernameAndPassword(@Param("username") String username, @Param("password") String password, @Param("tenant_code") String tenantCode);
 
-    @Select("select u.* from `user` u, tenant t where u.tenant_id = t.id and u.username = #{phone} and u.status = 1 and t.code = #{tenant_code} and u.delete_time is null")
+    @Select("select u.* from `user` u, tenant t where u.tenant_id = t.id and u.phone = #{phone} and u.status = 1 and t.code = #{tenant_code} and u.delete_time is null")
     User findByPhone(@Param("phone") String phone, @Param("tenant_code") String tenantCode);
 
     @Select("select u.* from user u, tenant t where u.tenant_id = t.id and u.openid=#{openid} and u.status=1 and t.code=#{tenant_code} and u.delete_time is null")
